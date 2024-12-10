2ff0876a06bd973f8adeb6ec8749464386a6effc
Sunghyun Kim <hello@sunghyun.me>
2024-12-11 00:17:23 +0900

    Initial commit

--- HeliosLauncher/app/assets/js/distromanager.js
+++ HeliosLauncher/app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Kevin-Studio-Dev/SpearfishForest4-Launcher-New/refs/heads/master/publishing/distrometa.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
